cce6b55a4009e13c17363797198b6f2a6aa921fb
shashkovsystemservice-coder <shashkov.systemservice@gmail.com>
2026-09-03 00:27:20 +0300

    Load conversion path styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./atmosphere.css";
 import "./about-contrast.css";
+import "./conversion-paths.css";
 import { siteConfig } from "./site-config";
 import AnalyticsTracker from "./AnalyticsTracker";
 
