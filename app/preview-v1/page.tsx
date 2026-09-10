b7a4ecef1bbbab387d219e245feaa659da7a9266
shashkovsystemservice-coder <shashkov.systemservice@gmail.com>
2026-09-10 23:36:10 +0300

    Add preview route for v1 conversion changes

---
+++ app/preview-v1/page.tsx
@@ -0,0 +1,12 @@
+import Home from "../page";
+import PreviewEnhancements from "./PreviewEnhancements";
+import "./preview.css";
+
+export default function PreviewV1Page() {
+  return (
+    <>
+      <Home />
+      <PreviewEnhancements />
+    </>
+  );
+}
